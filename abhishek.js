12166e6d809def2bb45d6b4fbb9079a457da35fa
apurbasbjk30 <apurba131223@gmail.com>
2024-12-26 21:59:21 +0530

    updated

---
+++ abhishek.js
@@ -0,0 +1,10 @@
+
+
+// header
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
