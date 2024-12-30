dc07668cdd4e680f388281ca1c69a8b308cb727b
apurbasbjk30 <apurba131223@gmail.com>
2024-12-30 14:31:28 +0530

    about updated

--- apurba_script.js
+++ apurba_script.js
@@ -62,3 +62,35 @@ arrowRight.addEventListener("click", function() {
 });
 
 startSlide();
+
+
+
+
+// about_right 
+// tabs
+
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
+
+
+tabLinks.forEach(function(el) {
+   el.addEventListener("click", openTabs);
+});
+
+
+function openTabs(el) {
+   var btnTarget = el.currentTarget;
+   var tab = btnTarget.dataset.tab;
+
+   tabContent.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   tabLinks.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   document.querySelector("#" + tab).classList.add("active");
+   
+   btnTarget.classList.add("active");
+}
--- test.js
+++ test.js
@@ -1,49 +1,27 @@
-let sliderImages = document.querySelectorAll(".slide"),
-  arrowLeft = document.querySelector("#arrow-left"),
-  arrowRight = document.querySelector("#arrow-right"),
-  current = 0;
+// tabs
 
-// Clear all images
-function reset() {
-  for (let i = 0; i < sliderImages.length; i++) {
-    sliderImages[i].style.display = "none";
-  }
-}
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
 
-// Init slider
-function startSlide() {
-  reset();
-  sliderImages[0].style.display = "block";
-}
 
-// Show prev
-function slideLeft() {
-  reset();
-  sliderImages[current - 1].style.display = "block";
-  current--;
-}
-
-// Show next
-function slideRight() {
-  reset();
-  sliderImages[current + 1].style.display = "block";
-  current++;
-}
-
-// Left arrow click
-arrowLeft.addEventListener("click", function() {
-  if (current === 0) {
-    current = sliderImages.length;
-  }
-  slideLeft();
+tabLinks.forEach(function(el) {
+   el.addEventListener("click", openTabs);
 });
 
-// Right arrow click
-arrowRight.addEventListener("click", function() {
-  if (current === sliderImages.length - 1) {
-    current = -1;
-  }
-  slideRight();
-});
 
-startSlide();
+function openTabs(el) {
+   var btnTarget = el.currentTarget;
+   var country = btnTarget.dataset.country;
+
+   tabContent.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   tabLinks.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   document.querySelector("#" + country).classList.add("active");
+   
+   btnTarget.classList.add("active");
+}
