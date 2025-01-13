6084c2c2da2feeab895eda36768492fc26dd6ad2
apurbasbjk30 <apurba131223@gmail.com>
2025-01-13 13:40:14 +0530

    update

--- apurba_script.js
+++ apurba_script.js
@@ -1,14 +1,13 @@
-$("#search-icon").click(function() {
-    $(".nav").toggleClass("search");
-    $(".nav").toggleClass("no-search");
-    $(".search-input").toggleClass("search-active");
-  });
-  
-  $('.menu-toggle').click(function(){
-     $(".nav").toggleClass("mobile-nav");
-     $(this).toggleClass("is-active");
-  });
-  
+
+
+  $('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+
 
 //   images slider 
 
