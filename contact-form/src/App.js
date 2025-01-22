019f7b6dac73fa95bda0e08274941a08bf79b972
apurbasbjk30 <apurba131223@gmail.com>
2025-01-22 12:34:56 +0530

    up

---
+++ contact-form/src/App.js
@@ -0,0 +1,19 @@
+
+
+
+// src/App.js
+import React from 'react';
+import './App.css';
+import ContactForm from './ContactForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App</h1>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
+
