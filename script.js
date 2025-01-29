01c1b220c04efaee1764f7d88702cc3759644e62
apurbasbjk30 <apurba131223@gmail.com>
2025-01-29 23:36:13 +0530

    updated

--- script.js
+++ script.js
@@ -94,20 +94,19 @@ function openTabs(el) {
    btnTarget.classList.add("active");
 }
 
-
 // testimonial 
-$(document).ready(function() {
+// $(document).ready(function() {
  
-  $("#owl-example").owlCarousel({
-      itemsDesktop : [1499,4],
-      itemsDesktopSmall : [1199,3],
-      itemsTablet : [899,2],
-      itemsMobile : [599,1],
-      navigation : true,
-      navigationText : ['<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-left fa-stack-1x fa-inverse"></i></span>','<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-right fa-stack-1x fa-inverse"></i></span>'],
-  });
+//   $("#owl-example").owlCarousel({
+//       itemsDesktop : [1499,4],
+//       itemsDesktopSmall : [1199,3],
+//       itemsTablet : [899,2],
+//       itemsMobile : [599,1],
+//       navigation : true,
+//       navigationText : ['<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-left fa-stack-1x fa-inverse"></i></span>','<span class="fa-stack"><i class="fa fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-right fa-stack-1x fa-inverse"></i></span>'],
+//   });
   
-});
+// });
 $(document).ready(function(){
   $('.customer-logos').slick({
       slidesToShow: 6,
