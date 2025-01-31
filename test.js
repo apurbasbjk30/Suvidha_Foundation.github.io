3f9eb070e063cd1ae418082950e6527949d7c799
apurbasbjk30 <apurba131223@gmail.com>
2025-01-31 20:00:25 +0530

    updated icon

--- test.js
+++ test.js
@@ -1,7 +1,25 @@
-// Carousel Auto-Cycle
-$(document).ready(function() {
-    $('.carousel').carousel({
-      interval: 6000
-    })
+jQuery(document).ready(function($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 0,
+      autoplay: true,
+      dots:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
   });
-
+});
